71c705960b296f48576937dc9a54ed8d585c47c4
Markay <54555076+Markay12@users.noreply.github.com>
2023-07-01 14:04:40 -0700

    First Commit

---
+++ ApartmentItemSort.py
@@ -0,0 +1,45 @@
+## Python Item Sort for Apartment Moving
+# Broken into three categories: Need, Want, Have
+# Used to keep track of what we are bringing, what we want and need
+
+
+import csv
+
+def parse_data(filename):
+    total_price_mark = 0  # Variable to store the total price for Mark
+    total_price_tanishq = 0  # Variable to store the total price for Tanishq
+
+    with open(filename, 'r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            item = row['Item']
+            need = row['Need']
+            want = row['Want']
+            have = row['Have']
+            price = float(row['Price'].replace('$', ''))
+            mark = row['Mark']
+            tanishq = row['Tanishq']
+
+            print(f"Item: {item}")
+            print(f"Need: {need}")
+            print(f"Want: {want}")
+            print(f"Have: {have}")
+            print(f"Price: ${price:.2f}")
+
+            if mark == 'True' and tanishq == 'False':
+                total_price_mark += price  # Add the price to Mark's total
+            elif mark == 'False' and tanishq == 'True':
+                total_price_tanishq += price  # Add the price to Tanishq's total
+            elif mark == 'True' and tanishq == 'True':
+                total_price_mark += price / 2  # Add half the price to each person
+                total_price_tanishq += price/2
+
+            print()
+
+    print(f"Total Price (Mark): ${total_price_mark:.2f}")
+    print(f"Total Price (Tanishq): ${total_price_tanishq:.2f}")
+
+
+filename = 'ApartmentItems.csv'
+parse_data(filename)
+
